92a35d20f377b8db70a3ca17d40e69692f63b626
Tony <greybluesea@gmail.com>
2023-06-03 15:20:19 +1200

    ClerkAuth added & "new-item"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,24 +16,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={
-          inter.className +
-          " bg-slate-800 text-slate-100 container max-w-sm mx-auto p-4 flex flex-col justify-center min-h-screen"
-        }
-        style={{ marginTop: "-12svh" }}
-      >
-        {children}
-        <footer>
-          <ul className="fixed bottom-20 left-0 right-0 flex flex-col justify-center items-center ">
-            <li>NEXT.js (Server Actions)</li>
-            <li>Prisma + SQLite</li>
-            <li>
-              learned from Kyle(Web Dev Simplified), powered by greybluesea
-            </li>
-          </ul>
-        </footer>
-      </body>
+      <ClerkProvider>
+        <body
+          className={
+            inter.className +
+            " bg-slate-800 text-slate-100 container max-w-sm mx-auto p-4 flex flex-col justify-center min-h-screen"
+          }
+          style={{ marginTop: "-12svh" }}
+        >
+          {children}
+          <footer>
+            <ul className="fixed bottom-20 left-0 right-0 flex flex-col justify-center items-center ">
+              <li>NEXT.js (Server Actions)</li>
+              <li>Prisma + SQLite</li>
+              <li>ClerkAuth</li>
+              <li className="flex gap-2">
+                learned from Kyle(Web Dev Simplified), powered by greybluesea
+                <UserButton afterSignOutUrl="/" />
+              </li>
+            </ul>
+          </footer>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ const Home = () => {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl">Shopping List</h1>
+        <h1 className="text-2xl flex gap-2">Shopping List</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded-md hover:bg-slate-700 focus-within:bg-slate-700 outline-none "
-          href="/newitem"
+          href="/new-item"
         >
           New Item
         </Link>
